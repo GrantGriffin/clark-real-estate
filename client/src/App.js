a2bda86699175a567e35a8eeb61c46e167fb3273
Kevin Clark <naeaman@gmail.com>
2020-03-16 16:30:28 -0600

    added Home and agentpage applying routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Layout from './features/layout/Layout'
-import './App.css';
+import AgentPage from './features/agent/AgentPage';
+import Home from './features/Home'
+import { Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       <Layout />
+      <Switch>
+        <Route exact path='/' render={<Home />} />
+        <Route path='/' render={<AgentPage />} />
+      </Switch>
     </div>
   );
 }
