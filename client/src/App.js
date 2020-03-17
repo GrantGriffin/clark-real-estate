ee737d7e87d3e87cb52130acca65a5b114eb44a7
ggriffin <grant.griffin@berkadia.com>
2020-03-17 00:46:59 -0600

    added an auth redirect, removed unused code, fixed routes, refactored home to a folder, removed unused styling from Layout.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import Layout from './features/layout/Layout'
 import AgentPage from './features/agent/AgentPage';
-import Home from './features/Home'
-import { Route, Switch } from 'react-router-dom';
+import Home from './features/home/Home'
+import { Route, Switch, Redirect } from 'react-router-dom';
 
+// boolean toggling login state
+const loggedIn = true
 
+// TODO: Add auth to manage login state
 function App() {
   return (
-    <div className="App">
+    <div>
       <Layout />
       <Switch>
-        <Route exact path='/' render={Home} />
-        <Route path='/' render={AgentPage} />
+        <Route exact path='/'>
+          {loggedIn ? <Redirect to='/agent-page' /> : <Redirect to='/home' />}
+        </Route>
+        <Route path="/home" render={Home} />
+        <Route path='/agent-page' render={AgentPage} />
       </Switch>
     </div>
   );
